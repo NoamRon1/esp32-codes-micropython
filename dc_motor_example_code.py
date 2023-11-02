104f8add21a6e4775b67b1ae25758e5052a61bc6
NoamRon1 <146370694+NoamRon1@users.noreply.github.com>
2023-11-02 21:15:40 +0200

    Update dc_motor_example_code.py

--- dc_motor_example_code.py
+++ dc_motor_example_code.py
@@ -8,10 +8,19 @@ pin1 = Pin(5, Pin.OUT) # the pin that connected to in1
 pin2 = Pin(4, Pin.OUT) # the pin that connected to in2
 enable = PWM(Pin(2), 15000) # enable pin
 
-motor = Motor(pin1, pin2, enable, False, False, False) # setup 1 motor, if 2: change the first false to pin1 of motor b, and the second and the third falses as well
+motor = Single_Motor(pin1, pin2, enable) # setup 1 motor
+#two_motors = Two_Motors(pin1a, pin2a, enablea, pin1b, pin2b, enableb) # setup 2 motors as a pair
 
 
-speed = 100 # the speed of the motor is between -100 to 100. a negative number is a moving backward motor. if 0 the motor will stop
-motor.start(speed, True) # The True value is the indicator of whether you are talking on the first motor or the second (in this code I didn't set the second motor). True = first motor. False = second motor
+speed = 100 # The speed of the motor is between -100 to 100. a negative number is a moving backward motor. if 0 the motor will stop
+motor.start(speed) # This will start the motor forever at a speed of 100.
+#two_motors.start(speed, True) # The True value is the indicator of whether you are talking on the first motor or the second (in this code I didn't set the second motor). True = first motor. False = second motor
+
+
+during = 10 # the time of the motor will be on (in sec)
+motor.timemove(speed, during) # this code will start the motor for 10 sec and will stop it after
+#second_speed = 50
+#two_motors.timemove(speed, second_speed, during) # this will start one of the motors at 100 speed and the other for 50, for 10 sec and will stop it after
+
 
 # This code will start the motor and it will never stop it
